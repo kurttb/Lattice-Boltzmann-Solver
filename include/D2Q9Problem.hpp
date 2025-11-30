23ac9e4fd82a65afda594b9a348caf20c1523863
Ethan Stout <enstout@umich.edu>
2025-11-29 19:36:45 -0800

    Modified class header to use Kokkos

--- include/D2Q9Problem.hpp
+++ include/D2Q9Problem.hpp
@@ -1,13 +1,12 @@
 #ifndef D2Q9_PROBLEM
 #define D2Q9_PROBLEM
 
+#include <Kokkos_Core.hpp>
 #include <vector>
 #include <string>
 #include "GridTypes.hpp"
 #include "BoundaryConditions.hpp"
 
-
-using namespace std;
 
 namespace LBM {
 
@@ -20,22 +19,22 @@ namespace LBM {
 			CartesianGrid2D _gridObj; 
 
 			// Body Forces
-			double _Fx = 0.0;
-			double _Fy = 0.0;
+			float _Fx = 0.0;
+			float _Fy = 0.0;
 
 			// Number of time steps
 			size_t _Nt = 50000;
 
 			// Fields
-			vector<double> _rho;
-			vector<double> _ux;
-			vector<double> _uy;
+			Kokkos::View<float*> _rho;
+			Kokkos::View<float*> _ux;
+			Kokkos::View<float*> _uy;
 
 			// Viscosity
-			double _nu;
+			float _nu;
 
 			// Distribution function
-			vector<double> _f;
+			Kokkos::View<float**> _f;
 
 			// Define Lattice - Start at rest, go east, and move counterclockwise
 			static constexpr int _ex[9] = {0, 1, 1, 0, -1, -1, -1, 0, 1};
@@ -43,7 +42,7 @@ namespace LBM {
 			static constexpr double _w[9] = {4.0/9.0, 1.0/9.0, 1.0/36.0, 1.0/9.0, 1.0/36.0, 1.0/9.0, 1.0/36.0, 1.0/9.0, 1.0/36.0}; // Weights for Maxwellian Distribution
 
 			// Write path
-			string _filePath;
+            std::string _filePath;
 
 			// Boundary Conditions
 			BCData _BCTop;
@@ -56,20 +55,20 @@ namespace LBM {
 
 		// Constructor/Destructor
 		D2Q9Problem(const size_t Nx, const size_t Ny);
-		~D2Q9Problem();
+		~D2Q9Problem() = default;
 
 		// Pre-processing functions
-		void setIC(const double rho0, const double ux0, const double uy0);
-		void setForces(const double Fx, const double Fy);
+		void setIC(const float rho0, const float ux0, const float uy0);
+		void setForces(const float Fx, const float Fy);
 		void setNumTimeSteps(const size_t Nt);
-		void setBC(const string& BCName, const string& BCType, const double uT = 0.0);
-		void setViscosity(const double nu);
+		void setBC(const std::string& BCName, const std::string& BCType, const float uT = 0.0);
+		void setViscosity(const float nu);
 
 		// Run the simulation
 		void runSimulation();
 
 		// Post-process
-		void writeOutput(string filePath);
+		void writeOutput(std::string filePath);
 
 	};
 
